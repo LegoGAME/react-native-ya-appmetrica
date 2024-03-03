f9734be0e9ff46f4372a9519ca8d14c529fd0562
LegoGAME <i.hate.radeon@gmail.com>
2024-03-03 15:05:03 +0300

    feat: export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,11 @@
 export * from './AppMetrica';
 export * from './Crashes';
+
+export type {
+  AppMetricaConfig,
+  RevenueInfo,
+  UserProfile,
+  CrashesConfiguration,
+  Location,
+  PreloadInfo,
+} from './types';
